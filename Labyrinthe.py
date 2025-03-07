f109ebeb8b631b3619b143eb939ed0b7bf75ffd3
E4lix <Ealix95@gmail.com>
2025-03-07 11:08:38 +0100

    Implémentation Labyrinthe

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -3,3 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mazelib import Maze
 from mazelib.generate.Prims import Prims
+
+# Random Maze
+m = Maze()
+m.generator = Prims(10, 10)
+m.generate()
+maze_grid = m.grid
+
+# Print the Maze Grid
+for row in maze_grid:
+    print(" ".join(str(cell) for cell in row))
+
+# Visualize the Maze
+def visualize_maze(grid):
+    plt.imshow(grid, cmap="binary")
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Generated Maze")
+    plt.show()
+
+# Main
+visualize_maze(maze_grid)
